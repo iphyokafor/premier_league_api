8a59bcff754ddafdc32ff40fb173638712a5e592
iphyokafor <sandraifeoma22@gmail.com>
2022-07-21 21:59:46 +0100

    setup babel

---
+++ src/db/seed/users/index.ts
@@ -0,0 +1,26 @@
+export =[
+  {
+    email: 'sandraifeoma@hotmail.com',
+    name: 'Melanie Marie',
+    password: 'password1234',
+    role: 'admin',
+  },
+  {
+    email: 'cassie@example.com',
+    name: 'Cassie Jang',
+    password: 'password1234',
+    role: 'user',
+  },
+  {
+    email: 'grace@example.com',
+    name: 'Grace Hope',
+    password: 'password1234',
+    role: 'admin',
+  },
+  {
+    email: 'sukang@example.com',
+    name: 'Sukang Lee',
+    password: 'password1234',
+    role: 'user',
+  },
+];
