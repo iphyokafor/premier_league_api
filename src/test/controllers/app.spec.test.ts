2874adb21656811c134d37ab4038c63d0ee0585e
iphyokafor <sandraifeoma22@gmail.com>
2022-07-26 11:31:50 +0100

    update tests

--- src/test/controllers/app.spec.test.ts
+++ src/test/controllers/app.spec.test.ts
@@ -1,8 +1,9 @@
 import chai from 'chai';
 import chatHttp from 'chai-http';
 import { app } from "../../app";
+import teamModel from '../../models/team.model';
 import userModel from '../../models/user.model';
-import { user, admin, user2 } from "./test.data";
+import { user, admin, user2, admin2, team, updateTeam } from "./test.data";
 
 
 chai.use(chatHttp);
@@ -10,11 +11,14 @@ const { expect } = chai;
 
 const url = "/api";
 
-let access_token: string;
+let access_token: string = '';
+let admin_access_token: string = '';
+let teamId: string = '';
 
 describe("it should test the mock premier league api", () => {
   before(async () => {
     await userModel.deleteMany({})
+    await teamModel.deleteMany({})
   });
 
   it("it should get the home page", async () => {
@@ -44,7 +48,6 @@ describe("it should test the mock premier league api", () => {
     expect(result.body.status).to.be.equal('success');
     expect(result.body.data.user).to.be.equal(result.body.data.user);
     expect(result.body.data.access_token).to.be.equal(result.body.data.access_token)
-    access_token = result.body.data.access_token
   });
 
   it("it should register an admin successfully", async () => {
@@ -56,7 +59,6 @@ describe("it should test the mock premier league api", () => {
     expect(result.body.status).to.be.equal('success');
     expect(result.body.data.admin).to.be.equal(result.body.data.admin);
     expect(result.body.data.access_token).to.be.equal(result.body.data.access_token)
-    access_token = result.body.data.access_token
   });
 
   it('It should not signup a user with duplicate details', async () => {
@@ -78,6 +80,39 @@ describe("it should test the mock premier league api", () => {
     expect(result.body.status).to.be.equal('success');
     expect(result.body.access_token).to.be.equal(result.body.access_token)
     access_token = result.body.access_token
+  });
+
+  it('It should log in an admin', async () => {
+    const result = await chai.request(app)
+      .post(`${url}/auth/login`)
+      .set('Accept', 'application/json')
+      .send(admin2)
+    expect(result.status).to.equal(200);
+    expect(result.body.status).to.be.equal('success');
+    expect(result.body.access_token).to.be.equal(result.body.access_token)
+    admin_access_token = result.body.access_token
+  });
+
+  it('It should Add a Team', async () => {
+    const result = await chai.request(app)
+      .post(`${url}/teams/create`)
+      .set('Authorization', `Bearer ${admin_access_token}`)
+      .set('content-type', 'application/json')
+      .send(team)
+    expect(result.status).to.equal(201);
+    expect(result.body.status).to.be.equal('success');
+    expect(result.body.data.team).to.be.equal(result.body.data.team);
+    teamId = result.body.data.team.id
+  });
+
+  it('It should not create team with duplicate details', async () => {
+    const result = await chai.request(app)
+      .post(`${url}/teams/create`)
+      .set('Authorization', `Bearer ${admin_access_token}`)
+      .set('content-type', 'application/json')
+      .send(team)
+    expect(result.body.status).to.be.equal('fail');
+    expect(result.body.message).to.be.equal('Nick name already exist');
   });
 
 });
